773a425d1d3576eef8f9e79af79144c0e9bb0df1
Adam Karlsten <adamkarlsten@gmail.com>
2017-12-22 00:17:55 +0100

    Added UU, SU. Now fetching job title. TODO: GBG, UMEÅ, Fix LU

--- app2.js
+++ app2.js
@@ -14,27 +14,35 @@ var mahScraper = (fakultet, name) => {
         .set('name')
         .follow('@href')
         .find('.advanced-content')
+        .set({title: '.usr_befatt'})
         .find('h2+table')
         .contains('mah.se')
         .set({email: 'td+'})
-        .data((data) => savedData.push(data))
+        .data((data) => {
+          console.log(data)
+          savedData.push(data)
+        })
     ])
     .done(() => {
       fs.writeFileSync('fil.json', JSON.stringify(savedData, null, 4))
       var reader = fs.createReadStream('fil.json')
-      var writer = fs.createWriteStream(`${name}.csv`)
+      var writer = fs.createWriteStream(`Malmoe-${name}.csv`)
   
-      reader.pipe(jsonexport({rename: ['Namn', 'Epost']})).pipe(writer)
+      reader.pipe(jsonexport({rename: ['Namn', 'Befattning', 'Epost']})).pipe(writer)
     })
 }
 
 // helper function to parse spamspan which LU uses
-var emailParser = (file) => {
+var emailParser = (file, uni) => {
   var emails = JSON.parse(fs.readFileSync(`${file}`))
 
   emails.forEach((person) => {
     var oldmail = person.email
-    var newmail = oldmail.replace(/ \[dot\] /g, '.').replace(/ \[at\] /g, '@')
+    if(uni === 'lu') {
+      var newmail = oldmail.replace(/ \[dot\] /g, '.').replace(/ \[at\] /g, '@')
+    } else if (uni === 'uu') {
+      var newmail = oldmail.replace(/ \[dot\] /g, '.').replace(/\[AT-tecken\]/g, '@')
+    }
     person.email = newmail
     return person
   })
@@ -52,80 +60,115 @@ var luScraper = (fakultet, name) => {
         .find('.lucat-user')
         .set({
           name: '.name > a',
+          title: '.roles',
           email: '.spamspan'
         })
-        .data((data) => savedData.push(data))
+        .data((data) => {
+          console.log(data)
+          savedData.push(data)
+        })
     ])
     .done(() => {
       fs.writeFileSync('fil.json', JSON.stringify(savedData, null, 4))
-      emailParser('fil.json')
+      emailParser('fil.json', 'lu')
       var reader = fs.createReadStream('fil.json')
-      var writer = fs.createWriteStream(`${name}.csv`)
+      var writer = fs.createWriteStream(`Lund-${name}.csv`)
   
-      reader.pipe(jsonexport({rename: ['Namn', 'Epost']})).pipe(writer)
+      reader.pipe(jsonexport({rename: ['Namn', 'Befattning', 'Epost']})).pipe(writer)
     })
 }
 
-luScraper('v1000065', 'Institutionen för kommunikation och medier')
+//UU-institutioner: http://katalog.uu.se/
+var uuScraper = (fakultet, name) => {
+  let savedData = []
+  osmosis
+    .get(`http://katalog.uu.se/orginfo/?orgId=${fakultet}`)
+    .set([
+      osmosis
+        .find('.unitEmployeeListContainer li')
+        .contains('uu.se')
+        .set({
+          name: '.emp-name',
+          title: '.emp-title',
+          email: '.emp-email'
+        })
+        .data((data) => {
+          console.log(data)
+          savedData.push(data)
+        })
+    ])
+    .done(() => {
+      fs.writeFileSync('fil.json', JSON.stringify(savedData, null, 4))
+      emailParser('fil.json', 'uu')
+      var reader = fs.createReadStream('fil.json')
+      var writer = fs.createWriteStream(`Uppsala-${name}.csv`)
+  
+      reader.pipe(jsonexport({rename: ['Namn', 'Befattning', 'Epost']})).pipe(writer)
+    })
+}
 
+//SU-institutioner - SOCIOLOGI
+var suSocScraper = (name) => {
+  let savedData = []
+  osmosis
+    .get('http://www.sociology.su.se/om-oss/kontakt/personal')
+    .set([
+      osmosis
+        .find('table tr')
+        .find('td>a')
+        .set('name')
+        .follow('@href')
+        .find('.profile-info')
+        .contains('@sociology.su.se')
+        .set({
+          title: '.employee-title',
+          email: '.work-place-info a:last'
+        })
+        .data((data) => {
+          console.log(data)
+          savedData.push(data)
+        })
+    ])
+    .done(() => {
+      fs.writeFileSync('fil.json', JSON.stringify(savedData, null, 4))
+      var reader = fs.createReadStream('fil.json')
+      var writer = fs.createWriteStream(`${name}.csv`)
+  
+      reader.pipe(jsonexport({rename: ['Namn', 'Befattning', 'Epost']})).pipe(writer)
+    })
+}
 
+// SU - STATSVETENSKAP
+var suStatsScraper = (name) => {
+  let savedData = []
+  osmosis
+    .get('http://www.statsvet.su.se/om-oss/kontakt/all-personal')
+    .set([
+      osmosis
+        .find('table tr')
+        .set({
+          name: 'a:first',
+          title: 'td:skip-first(1)',
+          email: 'a:last'
+        })
+        .data((data) => {
+          console.log(data)
+          savedData.push(data)
+        })
+    ])
+    .done(() => {
+      fs.writeFileSync('fil.json', JSON.stringify(savedData, null, 4))
+      var reader = fs.createReadStream('fil.json')
+      var writer = fs.createWriteStream(`${name}.csv`)
+  
+      reader.pipe(jsonexport({rename: ['Namn', 'Befattning', 'Epost']})).pipe(writer)
+    })
+}
 
-// var x = Xray().throttle(2, 1000)
-
-// // var reader = x('http://forskning.mah.se/org/fak/5', '#Toggle', ['b'], [{
-// //   name: 'a',
-// //   link: 'a@href'
-// // }])
-// //   .stream(console.log())
-
-
-// // var writer = fs.createWriteStream('names.csv')
-
-// // var options = {
-// //   rowDelimiter: '',
-// //   headers: ['Namn']
-// // }
-// // reader.pipe(jsonexport(options)).pipe(writer)
-// var fetchNames = () => {
-//   try {
-//     var namesandlinks = fs.readFileSync('namesandlinks.json')
-//     return JSON.parse(namesandlinks)
-//   } catch (e) {
-//     return []
-//   }
-// }
-
-
-// var csvconvert = () => {
-//   var reader = fs.createReadStream('namesandlinks.json')
-//   var writer = fs.createWriteStream('out.csv')
-//   reader.pipe(jsonexport()).pipe(writer)
-// }
-
-// var scrape = () => {x('http://forskning.mah.se/org/fak/5', '#Toggle', [{
-//   name: ['b a'],
-//   link: ['b a@href']
-// }])((err, res) => {
-//   if(err) {
-//     console.log(err)
-//   } else {
-//     fs.writeFileSync('namesandlinks.json', JSON.stringify(res))
-
-//     var links = {res}
-//     links = links.res[0].link
-
-//     links.forEach((link) => {
-//       x(link, '.advanced-content', [{
-//         email: 'h2+table td+'
-//       }]) ((err, res) => {
-//         var names = fetchNames()
-//         var email = res 
-//         names.push(email)
-//         saveFile(names)
-//       })
-//     })
-//   }
-// })}
-
-// scrape()
-
+// mahScraper('5', 'Institutionen för globala politiska studier (GPS)')
+// luScraper('v1000065', 'Institutionen för kommunikation och medier') // - KANSKE ÄR FEL
+// uuScraper('HS13:1', 'Statsvetenskapliga institutionen')
+// uuScraper('HS11:1', 'Sociologiska institutionen')
+// uuScraper('HS3', 'Institutionen för freds- och konfliktforskning')
+// suSocScraper('Uppsala - Sociologiska institutionen') //Bara sociologiska
+// suStatsScraper('Uppsala - Statsvetenskapliga institutionen') //Bara stats
